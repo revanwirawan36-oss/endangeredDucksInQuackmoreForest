63646bae9b8d0c45b0bda85ed32285a18a311458
revan wirawan <revanwirawan36@gmail.com>
2025-12-04 17:59:46 +0700

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,65 @@
+#include <ncurses/curses.h>
+#include "menu.h"
+#include <cstring>
+
+int drawMenuWindow(const char *title, const char *items[], int count) {
+    curs_set(0);
+    clear(); refresh();
+    int highlight = 0;
+    int ch;
+
+    int win_tinggi = 4 + count * 2 + 2;
+    int win_lebar  = 30;
+
+    int starty = (LINES - win_tinggi) / 2;
+    int startx = (COLS  - win_lebar)  / 2;
+
+    WINDOW *win = newwin(win_tinggi, win_lebar, starty, startx);
+    keypad(win, TRUE);
+    if (strcmp(title, "MAIN MENU") == 0) {
+    mvprintw(starty - 2, (COLS - 15) / 2, "CATCH THE DUCK");
+    refresh();
+    }
+    
+    while (1) {
+        werase(win);
+        box(win, 0, 0);
+
+        mvwprintw(win, 1, (win_lebar - strlen(title)) / 2, "%s", title);
+
+        for (int i = 0; i < count; i++) {
+            int posy = 3 + i * 2;
+
+            if (i == highlight)
+                wattron(win, A_REVERSE);
+
+            mvwprintw(win, posy, (win_lebar - strlen(items[i])) / 2, "%s", items[i]);
+
+            if (i == highlight)
+                wattroff(win, A_REVERSE);
+        }
+
+        wrefresh(win);
+
+        ch = wgetch(win);
+
+        if (ch == KEY_UP) {
+            highlight--;
+            if (highlight < 0)
+                highlight = count - 1;
+        }
+        else if (ch == KEY_DOWN) {
+            highlight++;
+            if (highlight >= count)
+                highlight = 0;
+        }
+        else if (ch == '\n') {
+            delwin(win);
+            return highlight;
+        }
+        else if (ch == 27) { // ESC
+            delwin(win);
+            return -1;
+        }
+    }
+}
