f36025e73f9728cd1aeccc40dd84004020e92fac
afifah raidhatu nasya <afifahnasya702@gmail.com>
2025-12-08 10:19:26 +0700

    Add files via upload

---
+++ HEADERS/leaderboard.h
@@ -0,0 +1,15 @@
+#ifndef LEADERBOARD_H
+#define LEADERBOARD_H
+
+#define MAX_ENTRIES 10 
+
+
+struct ScoreEntry {
+    char nama[21]; 
+    int skor;
+    int level; //0 easy 1 hard
+};
+
+void showLeaderboard();
+
+#endif 
